5278d6d6cf03a2fddf7f7dc3f310fcca12d7e8c9
eric fang <ericfang1992@gmail.com>
2019-06-27 08:42:43 -0700

    Info Page hours working, hard coded photo

--- dojo_parks/apps/parks/views.py
+++ dojo_parks/apps/parks/views.py
@@ -49,17 +49,17 @@ def create(request):
         website = res2['result']['website']
         rating = res2['result']['rating']
 
-        # Get the photo
-        photo_id = res2['result']['photos'][0]['photo_reference']
-        params2 = {
-            "place_id": place_id,
-            "key": "AIzaSyDcuEo_YNfM-UN8VWL9IeXtfJHR30R4I_0",
-            "photo_reference": photo_id,
-            "maxwidth": 400
-        }
-        raw_image_data = requests.get(placesapi, params=params2)
-        print(raw_image_data)
-        print(photo_id)
+        # # Get the photo
+        # photo_id = res2['result']['photos'][0]['photo_reference']
+        # params2 = {
+        #     "place_id": place_id,
+        #     "key": "AIzaSyDcuEo_YNfM-UN8VWL9IeXtfJHR30R4I_0",
+        #     "photo_reference": photo_id,
+        #     "maxwidth": 400
+        # }
+        # raw_image_data = requests.get(placesapi, params=params2)
+        # print(raw_image_data)
+        # print(photo_id)
 
         try:
             x = res2['result']['opening_hours']
@@ -101,29 +101,35 @@ def create(request):
 
 def parkinfo(request, parkid):
 
-    park = Park.objects.get(id=parkid)
-    # print(park.operating_hours)  # an array of 7 strings
-    hours_str = park.operating_hours
-    split_list = hours_str.split(",")
-    # print(split_list)
+    park = Park.objects.get(id=parkid) #Get the park from the parkid
+    hours_str = park.operating_hours #Get the string for park operating hours
+    print(hours_str)
+    if hours_str == "No hours listed":
+        left_bracket = "No hours listed"
+        right_bracket= ""
+        split_list = []
+    else:
+        split_list = hours_str.split(",") #Split the string into a list of strings, separated by commas
 
-    for word in split_list:
-        if "[" in word:
-            left_bracket = word.replace("[", "")
-    for word in split_list:
-        if "]" in word:
-            right_bracket = word.replace("]", "")
-    for word in split_list:
-        if "'" in word:
-            quotation = word.replace("'", "")
+        #Replace [] and ' with empty string
+        for word in split_list:
+            if "[" in word:
+                left_bracket = word.replace("[", "") #Only returns Monday without [
+        for word in split_list:
+            if "]" in word:
+                right_bracket = word.replace("]", "") #Only returns Sunday without ]
+        for word in split_list:
+            if "'" in word:
+                quotation = word.replace("'", "") #Not working
+        
+        split_list = split_list[1:-1]  # cutting off Monday and Sunday
+
+    # 2 different ways trying to remove beginning and ending quotation
     # for i in range(len(split_list)):
     #     if split_list[i] == "'":
     #         quotation = split_list[i].replace("'","")
     #     print(quotation)
 
-    split_list = split_list[1:-1]  # cutting off Monday and Sunday
-
-    # trying to remove beginning and ending quotation
     # for hour in split_list:
     #     split_list[hour].replace("'","")
     # print(split_list)
@@ -131,36 +137,39 @@ def parkinfo(request, parkid):
 
 
 
-    # Trying to get the photos from the places api
-    googlemapsapi = "https://maps.googleapis.com/maps/api/geocode/json"
-    params = {
-            'address': park.address,
-            'sensor': 'false',
-            'region': 'us',
-            'key': 'AIzaSyDcuEo_YNfM-UN8VWL9IeXtfJHR30R4I_0'
-        }
-    req = requests.get(googlemapsapi, params=params)
-    res = req.json()
-    place_id = res['results'][0]['place_id']
+    # # Trying to get the photos from the places api
+    # googlemapsapi = "https://maps.googleapis.com/maps/api/geocode/json"
+    # params = {
+    #         'address': park.address,
+    #         'sensor': 'false',
+    #         'region': 'us',
+    #         'key': 'AIzaSyDcuEo_YNfM-UN8VWL9IeXtfJHR30R4I_0'
+    #     }
+    # req = requests.get(googlemapsapi, params=params)
+    # res = req.json()
+    # place_id = res['results'][0]['place_id']
+    # print(place_id)
 
 
-    placesapi = "https://maps.googleapis.com/maps/api/place/details/json"
-    params = {
-            "place_id": place_id,
-            "key": "AIzaSyDcuEo_YNfM-UN8VWL9IeXtfJHR30R4I_0",
-        }
-    req2 = requests.get(placesapi, params=params)
-    res2 = req2.json()
+    # placesapi = "https://maps.googleapis.com/maps/api/place/details/json"
+    # params = {
+    #         "place_id": place_id,
+    #         "key": "AIzaSyDcuEo_YNfM-UN8VWL9IeXtfJHR30R4I_0",
+    #         # "fields" : "photo"
+    #     }
+    # req2 = requests.get(placesapi, params=params)
+    # res2 = req2.json()
+    # print(res2['result'])
 
-    # only get the 1st image, throwing a keyerror on 'photos'...works if this photo_id is hardcoded to any photo_reference
-    photo_id = res2['result']['photos'][0]['photo_reference'] 
+    # # only get the 1st image, throwing a keyerror on 'photos'...works if this photo_id is hardcoded to any photo_reference
+    # photo_id = res2['result']['photos'][0]['photo_reference'] 
     
     context = {
         "selected_park": park,
         "split_hours" : split_list,
         "formatted_hours" : left_bracket,
         "formatted_hours2" : right_bracket,
-        "photo_reference" : photo_id,
+        "photo_reference" : "CmRaAAAAeJXXDzKtE0O7LMEL9kHnY7Y4zXnLthg1t4yALnjd9yH7uQLmhH_LdbU5-kavzEQML3tpexaquvsegJ0ZnSlOlhVvAqenKIK5ozan_yzq80EL0CKdF6HZgqv3Pgd0ScXPEhAPNr_rr8pKKHy-K5NmlL4fGhRGGsDKuDl05nFJNropoAvCbkbjdQ",
         # "key" :  "AIzaSyDcuEo_YNfM-UN8VWL9IeXtfJHR30R4I_0" # don't have to pass in, can hardcode in html
     }
     return render(request, "parks/parkinfo.html", context)
